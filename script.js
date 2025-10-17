36ff031d480a4d6ed0233c8d53f1561d81853ccc
NKG-09 <200639623+NKG-09@users.noreply.github.com>
2025-10-17 11:09:39 +0530

    Refactor JavaScript entirely

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
-const stylesheet = document.styleSheets[0];
-
-const grid = document.querySelector(".square-grid");
-const size = document.querySelector(".size");
-const sizeDisplay = document.querySelector(".size-display");
-const colorPicker = document.querySelector(".color-picker");
-
-const colorButton = document.querySelector(".color");
-const eraserButton = document.querySelector(".eraser");
-const randomizeButton = document.querySelector(".randomize");
-const clearButton = document.querySelector(".clear");
-
 let isMouseDown = false;
-
 let brushState = 0;
+let selectedColor = "#000000";
+let selectedGridSize = 16;
 
 document.addEventListener("mousedown", () => isMouseDown = true);
 document.addEventListener("mouseup", () => isMouseDown = false);
 
+const grid = document.querySelector(".square-grid");
 grid.addEventListener("mousedown", (event) => { isMouseDown = true; paint(event); } );
 grid.addEventListener("mouseover", paint);
 
-size.addEventListener("input", () => sizeDisplay.textContent = `${size.value}x${size.value}`);
-size.addEventListener("change", () => generateGrid(size.value));
+document.querySelector(".options").addEventListener("click", editOptions);
+document.querySelector(".options").addEventListener("input", editOptions);
 
-colorButton.addEventListener("click", () => brushState = 0);
-eraserButton.addEventListener("click", () => brushState = 1);
-randomizeButton.addEventListener("click", () => brushState = 2);
-clearButton.addEventListener("click", () => generateGrid(size.value));
+generateGrid();
 
-generateGrid(16);
+function editOptions (event) {
+    const element = event.target;
+    
+    if (element.tagName === "BUTTON") {
+        if (element.textContent === "Clear") { generateGrid(); }
+        else brushState = ["Color", "Eraser", "Randomize"].indexOf(element.textContent);
+    }
+    
+    else if (element.tagName === "INPUT") {
+        if (element.type === "color") { selectedColor = element.value; }
+        else if (element.type === "range") {
+            selectedGridSize = element.value;
+            document.querySelector("#size-display").textContent = `${selectedGridSize}x${selectedGridSize}`;
+            if (event.type === "click") generateGrid();
+        }
+    }
+}
 
-function generateGrid(gridSize) {
+function generateGrid () {
     grid.innerHTML = '';
-    for (let i = 0; i < gridSize ** 2; i++) {
+    
+    for (let i = 0; i < selectedGridSize ** 2; i++) {
         const pixel = document.createElement("div");
         pixel.classList.add("pixel");
-        pixel.style.height = (100 / gridSize) + "%";
+        pixel.style.height = (100 / selectedGridSize) + "%";
         grid.appendChild(pixel);
     }
 }
@@ -48,14 +52,14 @@ function paint (event) {
     
     let color = "";
 
-    if (brushState === 0) color = colorPicker.value;
+    if (brushState === 0) color = selectedColor;
     if (brushState === 1) color = "white";
     if (brushState === 2) color = getRandomColor();
 
     pixel.style.backgroundColor = color;
 }
 
-function getRandomColor() {
+function getRandomColor () {
     const red = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
